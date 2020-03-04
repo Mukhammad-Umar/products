a323417b226e2470e372d4501544965c03f71895
Mukhammad-Umar <yoqubjonov98@mail.ru>
2020-03-04 08:13:44 +0000

    products

---
+++ console/clearcart.php
@@ -0,0 +1,11 @@
+<?php
+//require_once './controllers/CheckoutController.php';
+require_once './vendor/autoload.php';
+
+use CheckoutController\CheckoutController;
+
+$controller=new CheckoutController();
+
+//require_once './views/selectedaction.php';
+$controller->clearAll();
+?>
